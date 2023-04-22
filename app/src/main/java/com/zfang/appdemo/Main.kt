d0e6aef3a57a4f0634ec50cad98797c1e4721c1a
Little <414929680@qq.com>
2023-04-22 21:50:35 +0800

    [Add]OKHttp3 Demo

---
+++ app/src/main/java/com/zfang/appdemo/Main.kt
@@ -0,0 +1,27 @@
+package com.zfang.appdemo
+
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+
+
+fun main() {
+    testGet()
+}
+
+
+fun testGet() {
+    val url = "http://cf.colorbook.info/m/config?pubid=100194&moduleid=7070&pkg_name=com.colorbook.coloring&pkg_ver=1&file_ver=20171111"
+    val client = OkHttpClient()
+    val request = Request.Builder()
+        .url(url)
+        .build()
+    val call = client.newCall(request)
+    try {
+        val response = call.execute()
+        println(response.body?.string())
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+
+}
