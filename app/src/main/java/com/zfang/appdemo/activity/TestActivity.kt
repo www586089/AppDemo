9d58675487c4239a07b394152d513fba33af06f6
Little <414929680@qq.com>
2023-04-27 14:29:30 +0800

    [Add]用户引导完善

--- app/src/main/java/com/zfang/appdemo/activity/TestActivity.kt
+++ app/src/main/java/com/zfang/appdemo/activity/TestActivity.kt
@@ -9,7 +9,10 @@ import androidx.appcompat.widget.AppCompatTextView
 import com.zfang.appdemo.R
 import com.zfang.appdemo.activity.test.ScrollTestActivity
 import com.zfang.appdemo.base.BaseActivity
+import com.zfang.appdemo.common.px2Dp
 import com.zfang.appdemo.view.test.GuideInfo
+import com.zfang.appdemo.view.test.GuideInfoHelper
+import com.zfang.appdemo.view.test.TO_ANCHOR_BOTTOM
 import com.zfang.appdemo.view.test.TO_ANCHOR_LEFT
 import kotlinx.android.synthetic.main.activity_test.*
 
@@ -26,15 +29,45 @@ class TestActivity : BaseActivity() {
         initToolbar(title = "实验")
         supportActionBar?.hide()
         guideView.post {
-            val guideViewContent = LayoutInflater.from(this).inflate(R.layout.home_guide_layout_1, null)
-            guideViewContent.findViewById<AppCompatTextView>(R.id.guide_home_next).setOnClickListener {
-                guideView.showNext(image_test)
+            val guideViewContent1 = LayoutInflater.from(this).inflate(R.layout.home_guide_layout_1, null)
+            guideViewContent1.findViewById<AppCompatTextView>(R.id.guide_home_next).setOnClickListener {
+                guideView.showNext()
             }
-            guideView.setDisplayHollowGuidView(path_effect, guideViewContent, GuideInfo(toAnchorDirection = TO_ANCHOR_LEFT, scale = 0.235f))
+            val guideViewContent2 = LayoutInflater.from(this).inflate(R.layout.home_guide_layout_2, null)
+            guideViewContent2.findViewById<AppCompatTextView>(R.id.guide_home_know).setOnClickListener {
+                guideView.completeGuide()
+            }
+            val guideHomeNext = guideViewContent1.findViewById<AppCompatTextView>(R.id.guide_home_next)
+            val guideHomeComplete = guideViewContent2.findViewById<AppCompatTextView>(R.id.guide_home_know)
+            GuideInfoHelper.addGuideInfo(GuideInfo(anchorView = path_effect, clickView = guideHomeNext,
+                guideViewContent = guideViewContent1, toAnchorDirection = TO_ANCHOR_LEFT, scale = 0.235f, cornerRadius = 5.px2Dp(this).toFloat()))
+            GuideInfoHelper.addGuideInfo(GuideInfo(anchorView = image_test, clickView = guideHomeComplete,
+                guideViewContent = guideViewContent2, toAnchorDirection = TO_ANCHOR_BOTTOM, scale = 0.38f, cornerRadius = 5.px2Dp(this).toFloat()))
+
+            guideView.showGuide(GuideInfoHelper)
         }
     }
 
     fun onClickScrollTest(view: View) {
         ScrollTestActivity.start(this)
+    }
+
+    fun showNextGuide(view: View) {
+        val guideViewContent1 = LayoutInflater.from(this).inflate(R.layout.mine_guide_layout_1, null)
+        guideViewContent1.findViewById<AppCompatTextView>(R.id.guide_mine_next).setOnClickListener {
+            guideView.showNext()
+        }
+        val guideViewContent2 = LayoutInflater.from(this).inflate(R.layout.mine_guide_layout_2, null)
+        guideViewContent2.findViewById<AppCompatTextView>(R.id.guide_mine_know).setOnClickListener {
+            guideView.completeGuide()
+        }
+        val guideHomeNext = guideViewContent1.findViewById<AppCompatTextView>(R.id.guide_mine_next)
+        val guideHomeComplete = guideViewContent2.findViewById<AppCompatTextView>(R.id.guide_mine_know)
+        GuideInfoHelper.addGuideInfo(GuideInfo(anchorView = myEarn, clickView = guideHomeNext,
+            guideViewContent = guideViewContent1, toAnchorDirection = TO_ANCHOR_BOTTOM, scale = 0.016f, cornerRadius = 0.px2Dp(this).toFloat()))
+        GuideInfoHelper.addGuideInfo(GuideInfo(anchorView = taskCenter, clickView = guideHomeComplete,
+            guideViewContent = guideViewContent2, toAnchorDirection = TO_ANCHOR_BOTTOM, scale = 0.28f, cornerRadius = 18.px2Dp(this).toFloat(),gravity = 0.6f))
+
+        guideView.showGuide(GuideInfoHelper)
     }
 }
